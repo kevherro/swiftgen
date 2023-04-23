e44758c65e106f59e54a9cc0ee93eb122000b35f
Kevin Herro <kvherro@gmail.com>
2023-04-23 14:02:35 -0700

    tempfile: use standard library directly (#7)

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -15,11 +15,56 @@
 // Package driver implements the core swiftgen functionality.
 package driver
 
-import "github.com/kevherro/swiftgen/internal/codegen"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 
+	"github.com/kevherro/swiftgen/internal/schema"
+)
+
+// SwiftGen converts JSON schema data located at src to Swift code and writes
+// it to dest.
 func SwiftGen() error {
-	if err := codegen.Generate(); err != nil {
+	srcFlag := flag.String("src", "", "Path to JSON schema file")
+	destFlag := flag.String("dest", "", "Path to write location")
+
+	flag.Parse()
+
+	if *srcFlag == "" {
+		flag.Usage()
+		err := fmt.Errorf("driver: --src flag value not provided")
 		return err
 	}
+
+	src, err := os.Open(*srcFlag)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	b, err := io.ReadAll(src)
+	if err != nil {
+		return err
+	}
+
+	var s schema.JSONSchema
+	if err = json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(filepath.Dir(*destFlag), 0750); err != nil {
+		return err
+	}
+
+	g := schema.JSONSchemaToSwiftCodeGenerator{Schema: s}
+	c := g.Generate()
+	if err = os.WriteFile(*destFlag, []byte(c), 0666); err != nil {
+		return err
+	}
+
 	return nil
 }
--- internal/driver/driver_test.go
+++ internal/driver/driver_test.go
@@ -12,25 +12,23 @@
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
 
-package codegen
+package driver
 
 import (
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
 
 func TestGenerate(t *testing.T) {
-	// Create a temporary JSON schema file.
-	tempDirName := "/tmp/"
-	tempFile, err := os.CreateTemp(tempDirName, "schema.json")
+	f, err := os.CreateTemp(os.TempDir(), "schema.json")
 	if err != nil {
-		t.Fatal(err)
+		t.Errorf("CreateTemp(%v, %v) error = %v", os.TempDir(), "schema.json", err)
 	}
-	defer os.RemoveAll(tempDirName)
+	defer os.Remove(f.Name())
 
-	// Write the JSON schema to the temporary file.
-	jsonSchema := `{
+	blob := `{
 		"title": "Root",
         "type": "object",
         "properties": {
@@ -38,30 +36,28 @@ func TestGenerate(t *testing.T) {
             "age": { "type": "number" }
         }
     }`
-	if _, err := tempFile.Write([]byte(jsonSchema)); err != nil {
-		t.Fatal(err)
+	if _, err := f.Write([]byte(blob)); err != nil {
+		f.Close()
+		t.Errorf("f.Write() error = %v", err)
 	}
+	defer f.Close()
 
-	// Close the temporary file.
-	if err := tempFile.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	dest := "Schema.swift"
-
-	// Call Generate with the temporary file paths
+	// Call Generate with the temporary file paths.
+	dest := filepath.Dir(f.Name()) + "/" + "Schema.swift"
 	oldArgs := os.Args
-	defer func() { os.Args = oldArgs }()
-	os.Args = []string{"cmd", "--src", tempFile.Name(), "--dest", dest}
-	if err := Generate(); err != nil {
+	defer func() {
+		os.Args = oldArgs
+	}()
+	os.Args = []string{"cmd", "--src", f.Name(), "--dest", dest}
+	if err := SwiftGen(); err != nil {
 		t.Errorf("Generate() error = %v", err)
 	}
-	defer os.Remove(dest)
+	defer os.Remove(dest) // clean up
 
-	// Read the output file and check if it contains the generated code
+	// Read the output file and check if it contains the generated code.
 	generatedCode, err := os.ReadFile(dest)
 	if err != nil {
-		t.Fatal(err)
+		t.Errorf("ReadFile(%v) error = %v", dest, err)
 	}
 
 	expectedProperties := []string{
